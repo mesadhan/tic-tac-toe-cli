b35fcf12d71e21ca47256c22d0d5cc7756e73509
mesadhan <sadhansarker3@gmail.com>
2019-08-29 01:17:03 +0600

    draw game board properly

--- src/game-board/board.spec.js
+++ src/game-board/board.spec.js
@@ -2,12 +2,8 @@ const {
     drawGameBoard,
 } = require('../game-board/board');
 
-test('game control panel test start......', () => {
 
-    expect(true).toBe(true);
-});
-
-test('draw game board properly .....', () => {
+test('draw game board properly', () => {
 
     let expectedData = ` - | - | - \n-----------\n - | - | - \n-----------\n - | - | - `;
     let data = drawGameBoard();
