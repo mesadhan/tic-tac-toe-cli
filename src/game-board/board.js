cc3f7c784874ace6f189468abadf256ba589d102
mesadhan <sadhansarker3@gmail.com>
2019-08-29 01:14:03 +0600

    created game board with test cases

--- src/game-board/board.js
+++ src/game-board/board.js
@@ -1,9 +1,49 @@
+const {
+    clearConsole,
+} = require('../console-helper/utils');
 
+const board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
 
-const gameBoard = () => {
-    console.log('Message', ' Hello! From Game Board ');
+const drawGameBoard = () => {
+    clearConsole();
+
+    const n = 3;
+    let boardStr = '';
+    for (let i = 0; i < n; i += 1) {
+        for (let j = 0; j < n; j += 1) {
+            const curr = board[i][j];
+            if (curr === 'x') {
+                if (j === 1) {
+                    boardStr += '| X |';
+                } else {
+                    boardStr += ' X ';
+                }
+            } else if (curr === 'o') {
+                if (j === 1) {
+                    boardStr += '| O |';
+                } else {
+                    boardStr += ' O ';
+                }
+            } else {
+                if (j === 1) {
+                    boardStr += '| - |';
+                } else {
+                    boardStr += ' - ';
+                }
+            }
+        }
+        if (i !== 2) {
+            boardStr += '\n-----------\n';
+        }
+    }
+    return boardStr;
 };
 
+
 module.exports = {
-    gameBoard,
+    drawGameBoard,
 };
--- src/game-board/board.spec.js
+++ src/game-board/board.spec.js
@@ -1,5 +1,16 @@
+const {
+    drawGameBoard,
+} = require('../game-board/board');
 
 test('game control panel test start......', () => {
 
     expect(true).toBe(true);
+});
+
+test('draw game board properly .....', () => {
+
+    let expectedData = ` - | - | - \n-----------\n - | - | - \n-----------\n - | - | - `;
+    let data = drawGameBoard();
+
+    expect(expectedData).toBe(data);
 });
